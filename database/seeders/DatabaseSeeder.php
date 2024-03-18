53fd657af08f3a9b11ddcd0de9664e7e3d01be1d
dekolor <stefanbriceag1@gmail.com>
2024-03-18 17:04:44 +0200

    Updated database seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
 //        Awb::factory(10)->create();
-        AwbStep::factory(10)->create();
+//        AwbStep::factory(10)->create();
 
         Carrier::factory()->create([
            'name' => 'Sameday',
