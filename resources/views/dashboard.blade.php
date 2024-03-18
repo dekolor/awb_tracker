ba7b09f789e13fab961df751bcfc023bc77b2f67
dekolor <stefanbriceag1@gmail.com>
2024-03-18 12:18:29 +0200

    Small layout changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,13 +22,20 @@
                                             {{ $awb->awb_number }} ({{ $awb->tag }})
                                         </p>
                                         <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            {{ $awb->steps->sortBy('status_date')->last()->status_long ?? 'No status yet.' }}
+                                            @if($awb->steps->sortBy('status_date')->last())
+                                                {{ $awb->steps->sortBy('status_date')->last()->status_long }}
+                                                ({{ $awb->steps->sortBy('status_date')->last()->status_date }})
+                                            @else
+                                                No status yet.
+                                            @endif
                                         </p>
                                     </div>
                                     <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        <img height="32" width="32" class="mx-5" src="{{ $awb->carrier->logo }}" />
-                                        <a href="/awb/show/{{ $awb->id }}" class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold">Track</a>
-                                        <a href="/awb/delete/{{ $awb->id }}" class="mx-3 px-3 py-1 border border-red-300 rounded-full text-blue-300 text-xs uppercase font-semibold">Delete</a>
+                                        <img height="32" width="32" class="mx-5" src="{{ $awb->carrier->logo }}"/>
+                                        <a href="/awb/show/{{ $awb->id }}"
+                                           class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold">Track</a>
+                                        <a href="/awb/delete/{{ $awb->id }}"
+                                           class="mx-3 px-3 py-1 border border-red-300 rounded-full text-blue-300 text-xs uppercase font-semibold">Delete</a>
                                     </div>
                                 </div>
                             </li>
