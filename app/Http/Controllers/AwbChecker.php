c6f4ee107d40237d83c86522c29d20d822ee1df5
dekolor <stefanbriceag1@gmail.com>
2024-03-20 14:46:48 +0200

    Added error checking for sameday carrier

--- app/Http/Controllers/AwbChecker.php
+++ app/Http/Controllers/AwbChecker.php
@@ -59,18 +59,20 @@ class AwbChecker extends Controller
 
         $awbSteps = json_decode($response->body(), TRUE);
 
-        foreach ($awbSteps['awbHistory'] as $awbStep) {
-            AwbStep::updateOrCreate([
-                'awb_id' => $awb->id,
-                'county' => $awbStep['county'],
-                'country' => $awbStep['country'],
-                'status_long' => $awbStep['status'],
-                'status_code' => $awbStep['statusId'],
-                'status_short' => $awbStep['statusState'],
-                'status_state_code' => $awbStep['statusStateId'],
-                'transit_location' => $awbStep['transitLocation'],
-                'status_date' => $awbStep['statusDate']
-            ]);
+        if (!array_key_exists('error', $awbSteps)) {
+            foreach ($awbSteps['awbHistory'] as $awbStep) {
+                AwbStep::updateOrCreate([
+                    'awb_id' => $awb->id,
+                    'county' => $awbStep['county'],
+                    'country' => $awbStep['country'],
+                    'status_long' => $awbStep['status'],
+                    'status_code' => $awbStep['statusId'],
+                    'status_short' => $awbStep['statusState'],
+                    'status_state_code' => $awbStep['statusStateId'],
+                    'transit_location' => $awbStep['transitLocation'],
+                    'status_date' => $awbStep['statusDate']
+                ]);
+            }
         }
     }
 
